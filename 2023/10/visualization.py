e80289d503fb521e211320f084b2fbd33ad50942
Robert Main <ramain@mpifr-bonn.mpg.de>
2023-12-11 14:26:13 -0500

    gif creation in visualization script

--- 2023/10/visualization.py
+++ 2023/10/visualization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 vis = np.load("vis.npz")
 water = vis['vis']
@@ -12,3 +13,31 @@ plt.plot(xpath, ypath, c='b', lw=3.0, alpha=0.8)
 plt.xticks([])
 plt.yticks([])
 plt.show()
+
+print(len(xpath))
+# Generate gif
+step = 100
+for i in range(len(xpath)//step):
+    plt.figure(figsize=(5,5))
+    plt.plot(xpath[:i*step], ypath[:i*step], c='b', lw=1.0, alpha=0.8)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlim(0, water.shape[1])
+    plt.ylim(0, water.shape[0])
+    plt.savefig(f"plot_{i:03}.png")
+    plt.close()
+
+for i in range(water.shape[0]):
+    plt.figure(figsize=(5,5))
+    plt.plot(xpath, ypath, c='b', lw=1.0, alpha=0.8)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlim(0, water.shape[1])
+    plt.ylim(0, water.shape[0])
+    j = len(xpath)//step + i
+    plt.imshow(water[:i], cmap='Greys', interpolation='None')
+    plt.savefig(f"plot_{j:03}.png")
+    plt.close()
+
+os.system("convert -delay 5 -loop 1 plot*png pipe.gif")
+os.system("rm plot*png")
