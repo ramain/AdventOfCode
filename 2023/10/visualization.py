9e8cad917c3000b1760f1ad39c2153f1a79b3c0e
Robert Main <ramain@mpifr-bonn.mpg.de>
2023-12-13 15:54:09 -0500

    visualization changes

--- 2023/10/visualization.py
+++ 2023/10/visualization.py
@@ -19,7 +19,7 @@ print(len(xpath))
 step = 100
 for i in range(len(xpath)//step):
     plt.figure(figsize=(5,5))
-    plt.plot(xpath[:i*step], ypath[:i*step], c='b', lw=1.0, alpha=0.8)
+    plt.plot(xpath[:i*step], ypath[:i*step], c='k', lw=1.0, alpha=0.8)
     plt.xticks([])
     plt.yticks([])
     plt.xlim(0, water.shape[1])
@@ -29,13 +29,13 @@ for i in range(len(xpath)//step):
 
 for i in range(water.shape[0]):
     plt.figure(figsize=(5,5))
-    plt.plot(xpath, ypath, c='b', lw=1.0, alpha=0.8)
+    plt.plot(xpath, ypath, c='k', lw=1.0, alpha=0.8)
     plt.xticks([])
     plt.yticks([])
-    plt.xlim(0, water.shape[1])
-    plt.ylim(0, water.shape[0])
+    plt.xlim(0, water.shape[1]-1)
+    plt.ylim(0, water.shape[0]-1)
     j = len(xpath)//step + i
-    plt.imshow(water[:i], cmap='Greys', interpolation='None')
+    plt.imshow(water[:i], cmap='Blues', interpolation='None', vmin=-0.2, vmax=1)
     plt.savefig(f"plot_{j:03}.png")
     plt.close()
 
